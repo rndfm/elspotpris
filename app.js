4a94c1474de06d0180c6446dff9a41288726701a
Christian <ex-cra@akademikerpension.dk>
2022-09-07 12:28:24 +0200

    Add privacypolicy.html

--- app.js
+++ app.js
@@ -48,15 +48,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.get('/privatlivspolitik', function(req, res, next) {
+  req.url = "privacypolicy.html";
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'client/public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join('client/public', 'index.html'));
+router.get('*', function(req, res){
+  res.status(404).send('what???');
 });
-
 
 module.exports = router;
