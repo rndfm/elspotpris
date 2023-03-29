073d347dc6573043c84297ebc36e427b40798722
kr-moller <94559411+kr-moller@users.noreply.github.com>
2023-03-29 09:29:08 +0200

    Update prices.js
    Inkl. 2023 H2.
    Jf. https://fm.dk/media/26367/faktaark_lempelse-af-elafgift-til-minimumssats-i-seks-maaneder.pdf skifter afgift igen 2024.

--- elspotpris.app/src/prices.js
+++ elspotpris.app/src/prices.js
@@ -12,6 +12,13 @@ export const governmentTariffs = [
 		amount: 0.008,
 		validFrom: "2023-01-01T00:00:00",
 		validTo: "2023-07-01T00:00:00",
+	},
+	{
+		id: 'elafgift2023h2',
+		name: 'Elafgift 2023 H2',
+		amount: 0.697,
+		validFrom: "2023-07-01T00:00:00",
+		validTo: "2024-01-01T00:00:00",
 	}
 ];
 
